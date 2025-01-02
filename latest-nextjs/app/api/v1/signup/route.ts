c5b0bcb1a07bb5e7cb21ce6da4985558d711a837
code-it-vro <ashutosh2001shukla@gmail.com>
2025-01-02 20:10:21 +0530

    adding prisma connection

--- latest-nextjs/app/api/v1/signup/route.ts
+++ latest-nextjs/app/api/v1/signup/route.ts
@@ -1,8 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import { PrismaClient } from "@prisma/client";
+const prismaClient = new PrismaClient();
+
 export async function POST(req: NextRequest) {
   try {
     const data = await req.json();
+
+    await prismaClient.user.create({
+      data: {
+        name: data.name,
+        password: data.password
+      },
+    });
+
     console.log("Received data:", data);
 
     return NextResponse.json({
