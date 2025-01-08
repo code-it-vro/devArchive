3b328484605d0a96af7881b7bba58e16378c02dc
code-it-vro <ashutosh2001shukla@gmail.com>
2025-01-08 13:13:12 +0530

    adding new folder paytm project with database schema

---
+++ paytmyProject/backend/db.js
@@ -0,0 +1,12 @@
+
+import mongoose from "mongoose";
+import { password } from "pg/lib/defaults";
+
+const userSchema = new mongoose.Schema({
+    firstName: {type : String , required:true},
+    lastName: {type : String , required:true},
+    email: {type : String , required:true},
+    password: {type : String , required:true},
+})
+
+const User = mongoose.model("User", userSchema);
